ed6e7c859d70a4227e9aad655e0b7b7e863e458f
damar <damarisriverosg@gmail.com>
2022-05-11 17:52:35 -0400

    modelo

--- perfectoBastardo/musica/models.py
+++ perfectoBastardo/musica/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class TipoUsuario(models.Model):
+    idtipo_Usu = models.IntegerField(primary_key=True,verbose_name='Id')
+    tipoUsu = models.CharField(max_length=50,verbose_name='tipo')
+
+    def __str__(self):
+        return self.TipoUsu
+
+class Usuario(models.Model):
+    idusuario = models.IntegerField(primary_key=True,verbose_name='Id')
+    nombreUsuario = models.CharField(max_length=50,verbose_name='nombre')
+    Contrasenna = models.CharField(max_length=6,primary_key=True, verbose_name='contrasenna')
+    Fecha_nac = models.CharField(max_length=20,null=True, blank=True, verbose_name='Modelo')
+    usuario = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.categoria
